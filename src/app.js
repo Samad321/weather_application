90128c032a259b614ea42cc1c455c6194e2b8a35
samad <soma.payman@gmai.com>
2020-04-20 23:01:07 +0300

    sep app code for heroku

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const hbs = require("hbs");
 const geocode = require("./util/geocode");
 const forecast = require("./util/forecast");
 
-const app = express();
+const port =process.env.PORT ||3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -87,6 +87,6 @@ app.get("*", (req, res) => {
   res.send("404 error masasge");
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000.");
+app.listen(port, () => {
+  console.log("Server is up on port "+port);
 });
